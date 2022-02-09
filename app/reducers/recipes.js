5788dde5015b2a806c11060eed4d426d4b79873f
njanaskie <53629921+njanaskie@users.noreply.github.com>
2022-02-08 22:47:44 -0500

    add recipe to front of list

--- app/reducers/recipes.js
+++ app/reducers/recipes.js
@@ -7,8 +7,8 @@ const recipesReducer = (state = recipesReducerDefaultState, action) => {
     switch (action.type) {
         case 'ADD_RECIPE':
             return [
-                ...state,
-                action.recipe
+                action.recipe,
+                ...state
             ];
         case 'REMOVE_RECIPE':
             return state.filter(({ id }) => id !== action.id);
